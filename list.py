88fd776bb82c79723fc373e34138f86bed577c4a
Sanatbek <sanajonjafarov@gmail.com>
2025-10-16 14:40:29 +0500

    list 1 ni qoshish yangi kod

--- list.py
+++ list.py
@@ -2,16 +2,13 @@ import math
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
 print(thislist[0])
-
 print("Assalomu alaykum")
-
+print("245- guruh")
 print(thislist[2])
-print("gid hub+")
-print("gid hub +2")
-print("gid +")
-
-print("gid +2")
-print("gid hub +3")
-n = 5
-natija = math.factorial(n)
-print(f"{n}! = {natija}")
+massiv = [5,2,8,1,9,3]
+max_element = max(massiv)
+min_element = min(massiv)
+max_index = massiv.index(max_element)
+min_index = massiv.index(min_element)
+massiv[max_index], massiv[min_index] = massiv[min_index], massiv[max_index]
+print("natija:", massiv)
