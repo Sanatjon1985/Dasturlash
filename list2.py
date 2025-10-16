377b01243cefb7f856147561ea0bda18b35f3685
Sanatbek <sanajonjafarov@gmail.com>
2025-10-16 14:32:37 +0500

    list 2 ni qoshish

---
+++ list2.py
@@ -0,0 +1,10 @@
+
+
+massiv = [5,2,8,1,9,3]
+print("in:", massiv)
+max_element = max(massiv)
+min_element = min(massiv)
+max_index = massiv.index(max_element)
+min_index = massiv.index(min_element)
+massiv[max_index], massiv[min_index] = massiv[min_index], massiv[max_index]
+print("out:", massiv)
