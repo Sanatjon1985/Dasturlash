f9001f0ad7c8a72ddfe09213254b7db72c214b6f
Sanatbek <sanajonjafarov@gmail.com>
2025-10-16 14:23:25 +0500

    Yangi fayllar qoshildi

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+import math
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
 print(thislist[0])
@@ -10,8 +11,7 @@ print("gid hub +2")
 print("gid +")
 
 print("gid +2")
-
-
-
 print("gid hub +3")
-
+n = 5
+natija = math.factorial(n)
+print(f"{n}! = {natija}")
